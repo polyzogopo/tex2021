db1d1ab78f2bc3a86ff8864c2de260d7e1be15d0
silia6375 <66314575+silia6375@users.noreply.github.com>
2021-06-11 21:29:38 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+If you are already user of the system fill the following form, or press<br>
+ <a href='index.php?pg=registry.php'>Registry</a><br><br>
+<form action='index.php' method=post name=frm1>
+<table width=300>
+<tr><td>email:</td><td><input type=email required name=email></td></tr>
+<tr><td>Password:</td><td><input type=password required name=password></td></tr>
+<tr><td></td><td><input type=submit class='art-button' value='Login' name=button1></td></tr>
+ 
+</table>
+
+
+</form>
