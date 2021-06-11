42ec47d24d61e8a8cfa5c118c5f18cd8c532f50f
silia6375 <66314575+silia6375@users.noreply.github.com>
2021-06-11 21:21:20 +0300

    Add files via upload

---
+++ down.php
@@ -0,0 +1,34 @@
+
+		</div>
+</div>
+</div>
+                                
+                 
+
+</article></div>
+                    </div>
+                </div>
+            </div>
+    </div>
+<footer class="art-footer">
+  <div class="art-footer-inner">
+<div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 50%">
+        <p>Copyright © 2020. All Rights Reserved.</p>
+    </div><div class="art-layout-cell layout-item-0" style="width: 50%">
+        <p style="float:right"><a href="#">About</a>&nbsp;&nbsp; &nbsp;| &nbsp; &nbsp;<a href="#">Terms of Use</a>&nbsp;&nbsp; &nbsp;| &nbsp; &nbsp;<a href="#">Contact</a></p>
+    </div>
+    </div>
+</div>
+
+    <p class="art-page-footer">
+        <span id="art-footnote-links"><a href="http://www.artisteer.com/" target="_blank">Web Template</a></span>
+    </p>
+  </div>
+</footer>
+
+</div>
+
+
+</body></html>
