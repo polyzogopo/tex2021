c6363099fd0b567579318dff10b895c791c6d1e4
GeoKyrodimos <85258255+GeoKyrodimos@users.noreply.github.com>
2021-06-11 21:23:11 +0300

    Add files via upload

---
+++ freeforms.php
@@ -0,0 +1,12 @@
+<?php
+$s="select * from forma where free=0 order by category";
+$q=mysql_query($s);
+echo "<h2>Forms</h2>";
+while ($r=mysql_fetch_array($q))
+{
+echo "<a href=index.php?pg=showform.php&id=$r[id]>$r[title]</a> - Category:$r[category] <br>";
+
+ 
+}
+
+?>
