2f630eb2bc1d2fe5d90de2864de6a11867bec62d
silia6375 <66314575+silia6375@users.noreply.github.com>
2021-06-11 21:34:03 +0300

    Add files via upload

---
+++ registry.php
@@ -0,0 +1,16 @@
+
+Please fill the following form:<br><br>
+<form action='index.php?pg=registry2.php' method=post name=frm2>
+<table width=300>
+<tr><td>email*:</td><td><input type=email required name=email></td></tr>
+<tr><td>Password*:</td><td><input type=password required name=password></td></tr>
+<tr><td>Firstname:</td><td><input type=text  name=fname></td></tr>
+<tr><td>Lastname:</td><td><input type=text  name=lname></td></tr>
+<tr><td>Phone:</td><td><input type=text  name=phone></td></tr>
+<tr><td></td><td><input type=submit value='Registry' class='art-button' name=button2></td></tr>
+ 
+</table>
+<br>
+<i> * Required Fields </i>
+
+</form>
