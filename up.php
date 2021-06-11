c816f93b0349fc68dc323241e92efd7967633b44
silia6375 <66314575+silia6375@users.noreply.github.com>
2021-06-11 21:30:13 +0300

    Add files via upload

---
+++ up.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US"><head><!-- Created by Artisteer v4.1.0.59861 -->
+    <meta charset="utf-8">
+    <title>SEO Matters Home</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+ 
+    <!--[if lt IE 9]><script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="style.css" media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href="style.responsive.css" media="all">
+
+
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+    <script src="script.responsive.js"></script>
+
+
+<style>.art-content .art-postcontent-0 .layout-item-0 { padding: 20px;  }
+.art-content .art-postcontent-0 .layout-item-1 { padding-top: 20px;padding-right: 0px;padding-bottom: 0px;padding-left: 0px;  }
+.ie7 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+.ie6 .art-post .art-layout-cell {border:none !important; padding:0 !important; }
+
+</style></head>
+<body>
+<div id="art-main">
+<?php
+include ('menu.php');
+
+?>
+<header class="art-header">
+
+
+    <div class="art-shapes">
+<div class="art-object28755563" data-left="100%"></div>
+<div class="art-textblock art-object0" data-left="0%">
+        <div class="art-object0-text-container">
+        <div class="art-object0-text"></div>
+    </div>
+    
+</div>
+            </div>
+<h1 class="art-headline" data-left="8.15%">
+    <a href="#">GEOARCH.gr</a>
+</h1>
+<h2 class="art-slogan" data-left="4.9%">LandSlide Inventory Database</h2>
+
+
+
+
+                        
+                    
+</header>
+<div class="art-sheet clearfix">
+            <div class="art-layout-wrapper">
+                <div class="art-content-layout">
